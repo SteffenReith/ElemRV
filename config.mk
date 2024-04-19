84439ccc735be682b4ee2519dfe20bbf29931852
Steffen Reith <Steffen.Reith@hs-rm.de>
2024-04-19 20:17:41 +0200

    Initial (non-working) checkin

---
+++ config.mk
@@ -0,0 +1,11 @@
+export DESIGN_NAME = elem
+export PLATFORM    = ihp-sg13g2
+
+export VERILOG_FILES = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/src/SG13G2Top.v
+export SDC_FILE      = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
+
+export USE_FILL = 1
+
+export PLACE_DENSITY ?= 0.88
+export CORE_UTILIZATION = 20
+export TNS_END_PERCENT = 100
