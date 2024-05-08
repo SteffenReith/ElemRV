7efdc13509b8270ccb68b8921f22dfb2fe54feef
Daniel Schultz <d.schultz@phytec.de>
2024-05-08 11:23:22 +0200

    config: Revert path change
    The ORFS path is currently hard-coded while adding the bondpads.
    Move ElemRV back into ORFS.
    
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

--- config.mk
+++ config.mk
@@ -5,12 +5,12 @@ export PLATFORM        = ihp-sg13g2
 export IO_DIR = $(SG13G2_IO_DIR_PATH)/ihp-sg13g2/libs.ref/sg13g2_io/
 
 export VERILOG_FILES = \
-	${ELEMRV_HOME}/src/ElemRVTop.v \
+	./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/src/ElemRVTop.v \
 	$(IO_DIR)/verilog/sg13g2_io.v
 export SDC_FILE = \
-	${ELEMRV_HOME}/constraint.sdc
+	./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/constraint.sdc
 
-export SEAL_GDS = ${ELEMRV_HOME}/macros/sealring/sealring_1865x1867.gds
+export SEAL_GDS = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/macros/sealring/sealring_1865x1867.gds
 
 # Available area: 1920um x 1920um
 # Sealring: roughly 60um
@@ -32,14 +32,14 @@ export TNS_END_PERCENT = 100
 
 export PLACE_DENSITY = 0.70
 
-export FOOTPRINT_TCL = ${ELEMRV_HOME}/pad.tcl
-export PDN_TCL = ${ELEMRV_HOME}/pdn.tcl
+export FOOTPRINT_TCL = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/pad.tcl
+export PDN_TCL = ./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/pdn.tcl
 
 export ADDITIONAL_LEFS = \
-	$(OPENROAD_FLOW_ROOT)/platforms/$(PLATFORM)/lef/sg13g2_io.lef \
-	${ELEMRV_HOME}/macros/bondpad/bondpad_70x70.lef
+	./platforms/$(PLATFORM)/lef/sg13g2_io.lef \
+	./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/macros/bondpad/bondpad_70x70.lef
 export ADDITIONAL_GDS = \
-	$(OPENROAD_FLOW_ROOT)/platforms/$(PLATFORM)/gds/sg13g2_io.gds \
-	${ELEMRV_HOME}/macros/bondpad/bondpad_70x70.gds
+	./platforms/$(PLATFORM)/gds/sg13g2_io.gds \
+	./designs/$(PLATFORM)/$(DESIGN_NICKNAME)/macros/bondpad/bondpad_70x70.gds
 export ADDITIONAL_LIBS = \
 	$(IO_DIR)/liberty/sg13g2_io_dummy.lib
